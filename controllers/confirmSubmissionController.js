501c27b1cde2d9b73b02418a06a99891ccbee782
kelmanchen <kelmanchen@outlook.com>
2022-04-19 17:14:30 +1000

    re-added confirmSubmissionController

---
+++ controllers/confirmSubmissionController.js
@@ -0,0 +1,17 @@
+const display = (req, res) => {
+    const {glucose_data, comments} = req.body
+    res.render('submission_confirm', {glucose_data, comments})
+}
+
+const insert = (req, res) => {
+    const { glucose_data, comments } = req.body
+    glucoseData.push({ glucose_data, comments })
+    console.log(req.body)
+    return res.redirect('../record')
+}
+
+
+module.exports = {
+    display,
+    insert,
+} 
