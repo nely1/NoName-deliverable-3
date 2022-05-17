eb2bdf29d38ad63aab0b7d7901cd62b2b33d62fd
nely1 <nely@student.unimelb.edu.au>
2022-05-17 16:24:10 +1000

    trying to change dashboard data

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -1,14 +1,43 @@
 const patient = require('../models/patientModel')
 const clinician = require('../models/clinicianModel')
+const summary = require('../models/summaryModel')
 
 const display = async(req, res, next) => { 
 
-    const thisClinician = await clinician.findById("6282fa0e54139c4ed1b70231").lean().populate()
+    today = new Date()
+    if (today.getUTCHours() < 14) {
+        today.setUTCDate(today.getUTCDate()-1);
+    }
+    today.setUTCHours(14,0,0,0);
 
-    console.log(thisClinician)
+    const thisClinician = await clinician.findById("6282fa0e54139c4ed1b70231").lean()
 
-    const thisPatient = await patient.findById("62675c0d652ecfc70bd91d90").lean().populate()
-    res.render('dashboard', {data: thisPatient, dashboard: "active"})
+    let patientArray = thisClinician.patients
+    
+    let patientDetails = []
+    let summaries = []
+    for (var pat in patientArray) {
+        // add every patients' details to patientDetails
+        // add every summary that is done today and by a patient
+        // if the summary doesnt exist for a patient today add null to summaries
+
+        details = await patient.findById(patientArray[pat]).lean()
+        patientDetails.push(details)
+
+        summaryDetails = await summary.findOne({datetime: {$gte : today}, patientID: patientArray[pat]},{glucoseID: true, insulinID: true, weightID: true, exerciseID: true, _id: false})
+        .lean()
+        .populate('glucoseID insulinID weightID exerciseID') 
+        
+        if (!summaryDetails) {
+            patientDetails.summary = new summary()
+            
+        }
+        else {
+            patientDetails.summary = summaryDetails
+        }
+    }
+    console.log(patientDetails) 
+    res.render('dashboard', {patientData: patientDetails, dashboard: "active",summaryData: summaries})
 } 
 
 module.exports = {
